ccd360fcac8d708e6f99e48743afb0617a479cd7
JoshuaToback <joshuatoback@gmail.com>
2022-10-28 12:27:16 -0400

    Added Google API

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,9 @@
 var weatherApiRootUrl = 'https://api.openweathermap.org';
 var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
 
+var googlePlacesAPIRootURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDzsqpnaACAqQPCIPWwjt3yAA-Vyy29Z78&callback=initMap";
+var googlePlacesKey = 'AIzaSyDzsqpnaACAqQPCIPWwjt3yAA-Vyy29Z78'
+
 // DOM Elements
 var innerContainer = document.querySelector('#inner-container');
 var topBar = document.querySelector('#top-bar');
