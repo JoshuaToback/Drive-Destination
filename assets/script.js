cadff58dfb153d52c4fcaa372f3c67cffb7a33b4
JoshuaToback <joshuatoback@gmail.com>
2022-10-30 16:41:51 -0400

    Added Js and First Functions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
-// Global Variables
-var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+// // Global Variables
 
 var googlePlacesAPIRootURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDzsqpnaACAqQPCIPWwjt3yAA-Vyy29Z78&callback=initMap";
 var googlePlacesKey = 'AIzaSyDzsqpnaACAqQPCIPWwjt3yAA-Vyy29Z78'
@@ -8,9 +6,39 @@ var googlePlacesKey = 'AIzaSyDzsqpnaACAqQPCIPWwjt3yAA-Vyy29Z78'
 // DOM Elements
 var innerContainer = document.querySelector('#inner-container');
 var topBar = document.querySelector('#top-bar');
+var inputScreen = document.querySelector('#input-screen');
 var inputBox = document.querySelector('#inputBox');
 var roadScreen = document.querySelector('#roadScreen');
 
-// Timezone Plug-Ins
-dayjs.extend(window.dayjs_plugin_utc);
-dayjs.extend(window.dayjs_plugin_timezone);
+
+// Map Initializer
+let map;
+function createMap() {
+    map = new google.maps.Map(document.getElementById('map') , {
+        zoom: 6,
+        center: {}
+    })
+}
+
+// Search Functionality
+function getDriving(departureLocation, arrivalLocation) {
+    displayRoad();
+}
+
+// Hide Search Screen
+function displayRoad() {
+    inputScreen.setAttribute('#hide-screen');
+    roadScreen.setAttribute('#show-screen');
+};
+
+// AutoComplete
+let autocomplete;
+       function initAutocomplete() {
+        autocomplete = new google.maps.places.Autocomplete(
+          document.getElementById('autocomplete'),
+          {
+            types: ['establishment'],
+            componentRestrictions: { country: ['US']},
+            fields: ['place_id', 'geometry', 'name'],
+          })
+        }
