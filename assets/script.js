6d829ccb447408ddbe7bc17f941158c236cee3e0
JoshuaToback <joshuatoback@gmail.com>
2022-10-31 00:11:00 -0400

    Began distanceCalc function, commented out some extra event listeners.

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@
 
 var googlePlacesAPIRootURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDzsqpnaACAqQPCIPWwjt3yAA-Vyy29Z78&callback=initMap";
 var googlePlacesKey = 'AIzaSyDzsqpnaACAqQPCIPWwjt3yAA-Vyy29Z78'
+const cities = [ ];
 
 // DOM Elements
 var innerContainer = document.getElementById('inner-container');
@@ -11,6 +12,8 @@ var inputBox = document.getElementById('inputBox');
 var roadScreen = document.getElementById('road-screen');
 var storeLocation = localStorage.getItem('storeLocation')
 var driveBtn = document.getElementById('getDrive');
+// var departureLocation = document.getElementById('departureLocation');
+// var arrivalLocation = document.getElementById('arrivalLocation');
 
 // Map Initializer
 let map;
@@ -23,27 +26,57 @@ function createMap() {
 
 
 // AutoComplete
-let departureLocation;
-       function initAutocomplete() {
-        departureLocation = new google.maps.places.Autocomplete(
-          document.getElementById("departureLocation"),
-          {
-            types: ["establishment"],
-            componentRestrictions: { country: ["US"] },
-            fields: ["place_id", "geometry", "name"],
-          })
+function initAutocomplete() {
+  var departureLocations = new google.maps.places.Autocomplete(
+    document.getElementById("departureLocation"),
+      {
+        types: ["establishment"],
+        componentRestrictions: { country: ["US"] },
+        fields: ["place_id", "geometry", "name"],
+      })
 
-let arrivalLocation;          
-        arrivalLocation = new google.maps.places.Autocomplete(
-        document.getElementById("arrivalLocation"),
+  var arrivalLocations = new google.maps.places.Autocomplete(
+    document.getElementById("arrivalLocation"),
     {
       types: ["establishment"],
       componentRestrictions: { country: ["US"] },
       fields: ["place_id", "geometry", "name"],  
     });
-    console.log(departureLocation, arrivalLocation);
+
+    // google.maps.event.addListener(departureLocations, "location_Change", () => {
+    //   var departureLocation = departureLocations.getRoad()
+    //   var departureAddress = departureLocation.formatted_address
+    //   console.log(departureAddress);
+    // });
+
+    // google.maps.event.addListener(arrivalLocations, "location_Change", () => {
+    //   var arrivalLocation = arrivalLocations.getRoad()
+    //   var arrivalAddress = arrivalLocation.formatted_address
+    //   console.log(arrivalAddress);
+    // });
+};
+
+// // Distance Matrix
+function distanceCalc() {
+  var departure = $('#departure').val();
+  var arrival = $('#arrival').val();
+  var service = new google.maps.DistanceMatrixServ();
+  service.getDistanceMatrix(
+    {
+      origins: [origin], 
+      destinations: [destination],
+      travelMode: google.maps.TravelMode.DRIVING,
+      unitSystem: google.maps.unitSystem.IMPERIAL, 
+      avoidHighways: false,
+      avoidTolls: false,
+    },
+    callback()
+  );
 }
 
+// function callback (response, status) {
+//   if (status != google.maps.DistanceMatrixServ.OK)
+// }
 
 // Hide Search Screen
 function displayRoad() {
@@ -54,7 +87,6 @@ function displayRoad() {
 }
 
 function getRoad(departureLocation, arrivalLocation) {
-  
 };
 
 // Search Functionality
